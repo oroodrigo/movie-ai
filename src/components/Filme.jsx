c05b90f0f10012e1c6018b67135a5a7eea051a6f
Rodrigo Oliveira <rodrigofeitosa.contato@gmail.com>
2023-08-21 18:45:56 -0300

    update watchTrailer function

--- src/components/Filme.jsx
+++ src/components/Filme.jsx
@@ -2,15 +2,39 @@ import { Clock, CalendarBlank, Star } from "@phosphor-icons/react";
 
 const axios = require("axios");
 
-export default function Filme({
-  nome,
-  rate,
-  duracao,
-  lancamento,
-  poster,
-  id,
-  watchTrailer,
-}) {
+const watchTrailer = async (id) => {
+  const url = `https://api.themoviedb.org/3/movie/${id}/videos`;
+
+  try {
+    const res = await axios.get(url, {
+      headers: {
+        accept: "application/json",
+        Authorization:
+          "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiYWMyMWEyM2RiZWQzZmM3OTJkN2ZlM2ZjZDZmNDE0MCIsInN1YiI6IjY0ZDE2YTgzOTQ1ZDM2MDBhY2EwMzI5NiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.iUMf9iVxje3WDiDYi76Hx1tQg-KWJb6VZcZac9ORPvA",
+      },
+    });
+
+    const { results } = res.data;
+
+    const youtubeVideo = results.find((video) => video.type === "Trailer");
+
+    const isMobileDevice =
+      /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+        navigator.userAgent
+      );
+
+    if (isMobileDevice) {
+      const youtubeAppURL = `youtube://watch?v=${youtubeVideo.key}`;
+      window.location.href = youtubeAppURL;
+    } else {
+      window.open(`https://youtube.com/watch?v=${youtubeVideo.key}`, "_blank");
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export default function Filme({ nome, rate, duracao, lancamento, poster, id }) {
   return (
     <div className="flex flex-col max-w-[400px] gap-3 text-base-branco">
       <section className="flex justify-between ">
@@ -44,7 +68,9 @@ export default function Filme({
         </div>
       </section>
       <button
-        onClick={watchTrailer}
+        onClick={() => {
+          watchTrailer(id);
+        }}
         className="flex items-center w-full justify-center bg-base-cinza px-4 py-2 gap-2 rounded text-base-branco
         hover:bg-base-cinza-medio cursor-pointer"
       >
