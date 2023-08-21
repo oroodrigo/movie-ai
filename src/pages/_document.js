339387ffbe064c133f7256151d53ac1829d94053
Rodrigo Oliveira <rodrigofeitosa.contato@gmail.com>
2023-08-21 18:17:47 -0300

    update matatags

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,7 +4,13 @@ export default function Document() {
   return (
     <Html lang="pt-br">
       <Head>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link
+          rel="apple-touch-icon"
+          href="/apple-touch-icon.png"
+          type="image/png"
+          sizes="32x32"
+        />
       </Head>
       <body>
         <Main />
